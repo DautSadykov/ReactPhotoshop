f03c06f4b2443dd0e6eaabab6ac41c2cae3f1cfb
DautSadykov <sadykov.daut@yandex.ru>
2023-08-02 13:53:41 +0500

    upload image feature added

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,7 @@ export default function App() {
   const [options, setOptions] = useState(DEFAULT_OPTIONS)
   const [activeOptionIndex, setActiveOptionIndex] = useState(0)
   const activeOption = options[activeOptionIndex]
+  const [selectedImage, setSelectedImage] = useState(null);
   let sliderValue = 1
 
 
@@ -86,15 +87,22 @@ export default function App() {
     return { filter: filters.join(" ") }
   }
 
+  function handleImageUpload(event) {
+    const imageFile = event.target.files[0];
+    const imageUrl = URL.createObjectURL(imageFile);
+    console.log(imageUrl)
+    setSelectedImage(imageUrl);
+  }
+
   return(
     <div className="main">
       <div className="navbar_container">React Photoshop</div>
       <div className="photo_container">
-        <img 
-          className="image_to_edit" 
-          src="https://images.unsplash.com/photo-1679847727418-33ef58d86ebe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
-          style={getImageStyle()} 
-        />
+      <div className="photo_container">
+        {selectedImage && ( // Use selectedImage if available, otherwise use the default image
+          <img className="image_to_edit" src={selectedImage} style={getImageStyle()} alt="Uploaded" />
+        )}
+      </div>
       </div>
       <div className="slider_container">
         <input 
@@ -116,6 +124,17 @@ export default function App() {
             handleIndexChange = {handleIndexChange}
           />
         ))}
+      </div>
+      <div className="upload_container">
+        <label htmlFor="image-upload" className="upload_label">
+          Upload Image
+        </label>
+        <input
+          type="file"
+          id="image-upload"
+          accept="image/*"
+          onChange={handleImageUpload}
+        />
       </div>
     </div>
   )
